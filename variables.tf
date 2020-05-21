dbe09f55bf0d8995e43ec37dbd92d65de1da9b2e
bozmank <63351391+bozmank@users.noreply.github.com>
2020-05-21 09:57:57 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable aws_region {
 variable project_tag {
   description = "Value of the 'Project' tag for all resources"
   type        = string
-  default     = "Bozman1-terraform-run-triggers"
+  default     = "Bozman-Project tags"
 }
 
 variable vpc_cidr_block {
